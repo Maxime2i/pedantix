76fa434f9681b9844249f5b9dcd90d58de7fc7b9
Maxime <maxime.lngls21@gmail.com>
2025-06-04 12:53:15 +0200

    Ajout du fichier lexical_field.json avec Git LFS

--- backend/dico/download_lexical_field.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-DEST="dico/lexical_field.json"
-mkdir -p dico
-
-# Utilise gdown pour télécharger le fichier Google Drive
-python3 -m pip install --upgrade gdown
-gdown 1aZpXWFO6DIr7IxXN2sys9kNIhXG_HFe_ -O "$DEST" 
